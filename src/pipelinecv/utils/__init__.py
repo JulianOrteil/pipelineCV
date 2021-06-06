a0c76c944c6d09190a650fcd4e11d5651f6c9bb2
Julian_Orteil <>
2021-06-05 21:17:31 -0500

    Add logging configurer to module __init__

---
+++ src/pipelinecv/utils/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+Utilities, helper functions and configurations available to the entire
+application.
+"""
+
+
+__all__ = ['configure_logging']
+
+
+from .logging import configure_logging
