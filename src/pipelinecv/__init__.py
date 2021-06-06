0858553ecd6c22bdca27ad0973480e8a8ee94809
Julian_Orteil <>
2021-06-05 21:35:34 -0500

    Log metadata; remove start function

--- src/pipelinecv/__init__.py
+++ src/pipelinecv/__init__.py
@@ -14,6 +14,8 @@ from PySide6.QtWidgets import QApplication
 
 sys.path.append(os.path.abspath("./src"))
 
+from pipelinecv.utils import configure_logging
+
 
 __all__ = ['main']
 
@@ -33,14 +35,8 @@ class Application(QApplication):
 
     _start = Signal()
 
-    def exec(self) -> NoReturn:
-        raise NotImplementedError("use the start() method instead")
-
-    def exec_(self) -> NoReturn:
-        raise NotImplementedError("use the start() method instead")
-
-    def start(self) -> int:
-        """Start the application.
+    def exec(self) -> int:
+        """Run the application. Same as `exec_()`.
 
         Returns:
             errcode (`int`):
@@ -49,11 +45,23 @@ class Application(QApplication):
 
         # Emit the start signal. This signal is responsible for starting
         # the background loading processes.
+        logger.trace("Emitting start signal")
         self._start.emit()
 
         # Run the main event loop and return the errcode on application
         # exit
+        logger.debug("Entering application event loop")
         return super().exec()
+
+    def exec_(self) -> int:
+        """Run the application. Same as `exec()`.
+
+        Returns:
+            errcode (`int`):
+                The error code return from Qt's `QApplication.exec()`.
+        """
+
+        return self.exec()
 
 
 @logger.catch
@@ -67,8 +75,30 @@ def main() -> int:
             The error code return from Qt's `QApplication`.
     """
 
-    # Required by some multiprocessing modules and objects
+    # Required by some multiprocessing modules and objects. Per the
+    # docs, this SHOULD ALWAYS be the first thing executed in the main
+    # function.
     freeze_support()
+
+    # Configure logging first
+    configure_logging()
+
+    # Metadata logging
+    logger.info(f"Launching PipelineCV v{__version__}")
+    logger.info('-' * 100)
+    logger.info(f"Current working directory: {os.path.dirname(sys.argv[0])}")
+    logger.info(f"Logging file: {getattr(logger, 'logfile', 'N/A')}")
+    logger.info(f"Developed by: {__maintainer__}")
+    logger.info(f'Release Type: {__status__}')
+    logger.info('-' * 100)
+    logger.info("PIPELINECV IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR")
+    logger.info("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,")
+    logger.info("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE")
+    logger.info("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER")
+    logger.info("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,")
+    logger.info("OUT OF OR IN CONNECTION WITH PIPELINECV OR THE USE OR OTHER DEALINGS IN THE")
+    logger.info("SOFTWARE.")
+    logger.info('_' * 100)
 
     # Create the application object. This object is what handles all of
     # the window, core and supporting events and processes.
@@ -77,4 +107,4 @@ def main() -> int:
     app.setApplicationName("PipelineCV")
     app.setApplicationVersion(__version__)
 
-    return app.start()
+    return app.exec()
